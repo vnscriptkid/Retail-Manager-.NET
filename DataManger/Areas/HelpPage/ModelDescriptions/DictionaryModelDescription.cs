fee4ba5b51d140edca65abce05b95ebfa79eea6c
Thanh Nguyen <vnscriptkid@gmail.com>
2020-12-29 21:53:16 +0700

    Add DataManager Web API project

---
+++ DataManger/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DataManger.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ DataManger/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DataManger.WebApiApplication" Language="C#" %>
