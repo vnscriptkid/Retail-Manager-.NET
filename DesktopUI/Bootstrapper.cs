f0a5aaf0b69d2ff9d84744d08a3f5c25bb455aad
Thanh Nguyen <vnscriptkid@gmail.com>
2021-01-01 21:51:59 +0700

    Add login view and connect data between view and viewModel

--- DesktopUI/Bootstrapper.cs
+++ DesktopUI/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using DesktopUI.Helpers;
 using DesktopUI.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace DesktopUI
 {
@@ -16,6 +18,11 @@ namespace DesktopUI
         public Bootstrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+            PasswordBoxHelper.BoundPasswordProperty,
+            "Password",
+            "PasswordChanged");
         }
 
         protected override void Configure()
