81cdc4b476b5ba33c89e54119b7552d870a093ef
Thanh Nguyen <vnscriptkid@gmail.com>
2021-01-03 02:17:08 +0700

    Add lib for api layer and add get current user endpoint
    Lib that talks to db and returns data for api

---
+++ DataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,24 @@
+﻿using DataManager.Library.Internal.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataManager.Library.Models;
+
+namespace DataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id = Id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "RetailManagerDB");
+
+            return output;
+        }
+    }
+}
---
+++ DataManger/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using DataManager.Library.DataAccess;
+using DataManager.Library.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace DataManger.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Users")]
+    public class UserController : ApiController
+    {
+        // GET: Users/1
+        [Route("current")]
+        public List<UserModel> GetUserById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+        }
+    }
+}
