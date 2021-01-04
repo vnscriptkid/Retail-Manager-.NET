10e53113a3ede63d4eea58c3c0ef564a91b3a1f1
Thanh Nguyen <vnscriptkid@gmail.com>
2021-01-04 22:27:22 +0700

    Logic add to cart

--- DesktopUI/ViewModels/SalesViewModel.cs
+++ DesktopUI/ViewModels/SalesViewModel.cs
@@ -13,8 +13,8 @@ namespace DesktopUI.ViewModels
     public class SalesViewModel : Screen
     {
         private BindingList<ProductModel> _products;
-        private BindingList<ProductModel> _cart;
-        private int _itemQuantity;
+        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
+        private int _itemQuantity = 1;
         private IProductEndpoint _productEndpoint;
 
         public SalesViewModel(IProductEndpoint productEndpoint)
@@ -44,7 +44,7 @@ namespace DesktopUI.ViewModels
         }
 
 
-        public BindingList<ProductModel> Cart
+        public BindingList<CartItemModel> Cart
         {
             get { return _cart; }
             set
@@ -54,12 +54,26 @@ namespace DesktopUI.ViewModels
             }
         }
 
+        private ProductModel _selectedProduct;
+
+        public ProductModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set { 
+                _selectedProduct = value;
+                NotifyOfPropertyChange(() => SelectedProduct);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
+
+
         public int ItemQuantity
         {
             get { return _itemQuantity; }
             set { 
                 _itemQuantity = value;
                 NotifyOfPropertyChange(() => ItemQuantity);
+                NotifyOfPropertyChange(() => CanAddToCart);
             }
         }
 
@@ -67,18 +81,38 @@ namespace DesktopUI.ViewModels
         {
             get
             {
-                // TODO: add logic
-                return false;
+                return ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity;
             }
         }
 
-        public void AddToCart() { }
+        public void AddToCart() 
+        {
+            CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
+
+            if (existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
+                Cart.Remove(existingItem);
+                Cart.Add(existingItem);
+            }
+            else
+            {
+                CartItemModel item = new CartItemModel
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity
+                };
+                Cart.Add(item);
+            }
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            NotifyOfPropertyChange(() => SubTotal);
+            ItemQuantity = 1;
+        }
 
         public bool CanRemoveFromCart
         {
             get
             {
-                // TODO: add logic
                 return false;
             }
         }
@@ -89,8 +123,14 @@ namespace DesktopUI.ViewModels
         {
             get
             {
-                // TODO - Replace with calculation
-                return "$0.00";
+                decimal subTotal = 0;
+
+                foreach (var item in Cart)
+                {
+                    subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+                }
+
+                return subTotal.ToString("C");
             }
         }
 
